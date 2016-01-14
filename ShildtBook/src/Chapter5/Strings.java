6f8a3f5f252652bd1e9a1c317851880eebfa0210
unknown <ovo@sheridan.softservecom.com>
2016-01-14 17:10:35 +0200

    new methods encode in strings

--- ShildtBook/src/Chapter5/Strings.java
+++ ShildtBook/src/Chapter5/Strings.java
@@ -128,4 +128,26 @@ public class Strings {
             System.out.print(ch + " ");
         }
     }
+
+    public static void encriptionDemo(){
+        String msg = "This is a test";
+        String encmsg = "";
+        String decmsg = "";
+        int key = 88;
+        System.out.print("Original message: ");
+        System.out.println(msg);
+// зашифровать сообщение
+        for (int i=0; i < msg.length(); i++)
+// Построение зашифрованной строки сообщения,
+            encmsg = encmsg + (char) (msg.charAt(i) ^ key);
+        System.out.print("Encoded message: ");
+        System.out.println(encmsg) ;
+// дешифровать сообщение
+        for(int i=0; i < msg.length(); i++)
+// Построение дешифрованной строки сообщения.
+            decmsg = decmsg + (char) (encmsg.charAt(i) ^ key);
+        System.out.print("Decoded message: ");
+        System.out.println(decmsg);
+
+    }
 }
